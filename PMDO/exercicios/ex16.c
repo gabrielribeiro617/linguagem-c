f27a43f244fafa62992cb2ce970de0385cf28eae
Gabriel Gomes Ribeiro <gabriel617ribeiro@gmail.com>
2024-11-04 18:05:58 -0300

    att ex16

--- PMDO/exercicios/ex16.c
+++ PMDO/exercicios/ex16.c
@@ -5,12 +5,33 @@ int main(){
 
     printf("\033[1mDigite algo (scanf convencional): ");
     scanf("%s", texto);
-    fflush(stdin);
-    printf("\033[32mResultado: %s\033[m\n\n", texto);
-    printf("\033[1mDigite algo (scanf aprimorado): ");
+    fflush(stdin); // stdin é o teclado em linguagem C.
+    /* fflush(stdin) serve para que os próximos comandos de entrada - scanf, gets, fgets -, seja char, seja float, não deem
+    problema. */
+    puts("Resultado: ");
+    puts(texto);
+    puts("");
+
+    printf("Digite algo (scanf aprimorado): ");
     scanf("%9[^\n]s", texto);
     fflush(stdin);
-    printf("\033[32mResultado: %s\033[m", texto);
+    puts("Resultado: ");
+    puts(texto);
+    puts("");
+
+    printf("Digite algo (leitura pelo gets): ");
+    gets(texto);
+    fflush(stdin);
+    puts("Resultado: ");
+    puts(texto);
+    puts("");
+
+    printf("Digite algo (leitura pelo fgets): ");
+    fgets(texto, 10, stdin);
+    fflush(stdin);
+    puts("\033[32mResultado: ");
+    puts(texto);
+    puts("\033[m");
 
     return 0;
 }
