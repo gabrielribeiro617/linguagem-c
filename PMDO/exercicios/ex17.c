0cabd55418cdd4d2e7579113e2de1dadfdb25478
Gabriel Gomes Ribeiro <gabriel617ribeiro@gmail.com>
2024-11-04 19:46:55 -0300

    att ex17

--- PMDO/exercicios/ex17.c
+++ PMDO/exercicios/ex17.c
@@ -10,26 +10,44 @@ int main(){
     dá suporte para o uso de 'ç' e acentos. */
 
     char texto1[10] = "casa", texto2[10] = "-verde";
+    int i = 0;
 
-    puts(texto1);
+    puts("\033[1mInformacoes iniciais:");
+    printf("Texto1: %s\n", texto1);
+    printf("Texto2: %s\n\n", texto2);
+
     strcpy(texto1, "cha");
     /* A função strcpy é usada para alterar o conteúdo de uma
     string. */
     // strcpy ( <destino>, <origem> );
 
-    puts(texto1);
+    printf("Texto1 depois do strcpy: %s\n", texto1);
+
     strcat(texto1, texto2);
     /* A função strcat é usada para concatenar as duas strings
     dentro dos parâmetros, tendo o resultado no destino. */
     // strcat ( <destino>, <origem> );
 
-    puts(texto1);
-    // printf(strlen(texto1));
+    printf("Texto1 depois do strcat: %s\n", texto1);
+
+    i = strlen(texto1);
     // A função strlen é usada para saber o tamanho da string.
     // strlen ( <string> );
 
+    printf("Tamanho do Texto1 (strlen): %d letras\n", i);
+    puts("Texto1 espacado: ");
+    for (i = 0; i < strlen(texto1); i++) {
+        printf("%c ", texto1[i]);
+        // Imprimi cada caractere, da string texto 1, de forma espaçada.
+    }
+
+    if (strcmp(texto1, texto2) == 0) {
+        puts("\nO Texto1 e igual ao Texto2 (strcmp).\033[m");
+    } else {
+        puts("\nO Texto1 NAO e igual ao Texto2 (strcmp).\033[m");
+    }
     /* A função strcmp é usada para comparar duas strings, onde,
-    se forem iguais, o resultado é 0, mas se for falsa, não é. */
+    se forem iguais, o resultado é 0, mas se for falsa, não é 0. */
     // strcmp ( <string1>, <string2> );
     
     return 0;
