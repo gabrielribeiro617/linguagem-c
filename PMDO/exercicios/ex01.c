38e6c72e6705d324d67bec2d342a84808de7d4fd
Gabriel Gomes Ribeiro <gabriel617ribeiro@gmail.com>
2024-11-04 01:09:38 -0300

    aula 01 feita

--- PMDO/exercicios/ex01.c
+++ PMDO/exercicios/ex01.c
@@ -1,7 +1,20 @@
-#include <stdio.h>
+#include <stdio.h> 
+    /* Biblioteca que permite invocar funcionalidades e comandos
+    que são utilizados para que o usuário entenda o que está
+    acontecendo com o programa. */ 
+#include <stdlib.h> // Biblioteca para usar a função system.
 
+    /* Tudo o que estiver dentro das chaves, será executado
+    pela máquina. */
 int main(){
+    /* printf é uma função pré programada dentro da biblioteca
+    stdio.h que mostrará uma mensagem para o usuário. */
+    printf("Hello, World!\n");
+    system("pause"); 
+    /* Função da biblioteca stdlib.h que, quando executado o
+    arquivo .exe do programa, irá ficar pausado até que o
+    usuário clique em outra tecla. */
 
-    
     return 0;
 }
+    // Para executar o arquivo, ele deve ser, antes, salvo e compilado.
