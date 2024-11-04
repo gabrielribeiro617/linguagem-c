f78045d076234fc2528cd6ed9029eef92c62f2fc
Gabriel Gomes Ribeiro <gabriel617ribeiro@gmail.com>
2024-11-04 18:21:22 -0300

    att ex16

--- PMDO/exercicios/ex16.c
+++ PMDO/exercicios/ex16.c
@@ -8,12 +8,19 @@ int main(){
     fflush(stdin); // stdin é o teclado em linguagem C.
     /* fflush(stdin) serve para que os próximos comandos de entrada - scanf, gets, fgets -, seja char, seja float, não deem
     problema. */
+
     puts("Resultado: ");
+    /* A função puts imprimi uma string diretamente na tela, não
+    admitindo variáveis de outros tipos. */
+
     puts(texto);
     puts("");
 
     printf("Digite algo (scanf aprimorado): ");
     scanf("%9[^\n]s", texto);
+    // Tamanho: tamanho do vetor (10) - 1.
+    // [^\n]: para exibir o espaços em branco também.
+
     fflush(stdin);
     puts("Resultado: ");
     puts(texto);
@@ -21,6 +28,10 @@ int main(){
 
     printf("Digite algo (leitura pelo gets): ");
     gets(texto);
+    /* A função gets permite ler mais caracteres que o tamanho, já
+    definido, da string. */
+    // gets( <variável> );
+
     fflush(stdin);
     puts("Resultado: ");
     puts(texto);
@@ -28,8 +39,12 @@ int main(){
 
     printf("Digite algo (leitura pelo fgets): ");
     fgets(texto, 10, stdin);
+    /* A função fgets deixa o último caractere como reservado para
+    o '/0'. */
+    // fgets( <variável>, <tamanho>, <stdin> );
+
     fflush(stdin);
-    puts("\033[32mResultado: ");
+    puts("Resultado: ");
     puts(texto);
     puts("\033[m");
 
