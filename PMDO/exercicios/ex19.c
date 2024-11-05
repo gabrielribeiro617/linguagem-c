9de7750f158920e032dea80a05ec0c987982a5f6
Gabriel Gomes Ribeiro <gabriel617ribeiro@gmail.com>
2024-11-04 22:42:31 -0300

    att ex19 e ex20

--- PMDO/exercicios/ex19.c
+++ PMDO/exercicios/ex19.c
@@ -4,24 +4,27 @@
 
 #define TAM 50
 
-struct tipo_pessoa {
-    int idade;
+struct tipo_pessoa { // Define um novo tipo de dado, usando struct.
+    int idade; // Um dos 3 campos do tipo de dado tipo_pessoa.
     float peso;
     char nome[TAM];
 };
 
 typedef struct tipo_pessoa tipo_pessoa;
+    // Define um novo nome de struct tipo_pessoa -> tipo_pessoa.
 
 int main(){
     setlocale(LC_ALL, "Portuguese");
 
-    tipo_pessoa pes = {0, 0.0, "Teste"};
+    tipo_pessoa pes = {0, 0.0, "Teste"}; // define o nome da variável: pes
+    // Define também cada campo do tipo de dado.
 
     puts("Inicio:");
     printf("Idade: %d\n", pes.idade);
     printf("Peso: %.2f\n", pes.peso);
     printf("Nome: %s\n", pes.nome);
 
+    // Altera os dados dos campos:
     pes.idade = 10;
     pes.peso = 99.99;
     strcpy(pes.nome, "Texto");
@@ -33,6 +36,8 @@ int main(){
     printf("Nome: %s\n", pes.nome);
 
     puts("");
+
+    // Altera os dados dos campos usando comandos de entrada:
     puts("Insira a sua idade:");
     scanf("%d", &pes.idade);
     puts("Insira o seu peso:");
--- PMDO/exercicios/ex20.c
+++ PMDO/exercicios/ex20.c
@@ -17,30 +17,37 @@ int main(){
     setlocale(LC_ALL, "Portuguese");
 
     tipo_pessoa lista[TAM];
+    /* Define a variável como uma lista com 3 espaços, onde cada espaço
+    da lista conterá os três campos definidos (idade, peso, nome). */
+
     int i = 0;
 
+    /* Estrutura de repetição para definir cada campo de cada espaço da lista. */
     for (i = 0; i < TAM; i++) {
         printf("Insira os dados (%d):\n", i + 1);
         puts("Nome:");
-        scanf("%50[^\n]s", &lista[i].nome);
+        fgets(lista[i].nome, 50, stdin);
         fflush(stdin);
         puts("Idade:");
         scanf("%d", &lista[i].idade);
         puts("Peso:");
         scanf("%f", &lista[i].peso);
+        fflush(stdin);
     }
 
-    system("cls");
+    system("cls"); // Função da biblioteca stdlib.h que limpa a tela.
     puts("Seus dados:\n");
 
+    /* Estrutura de repetição para mostrar todos os dados contidos
+    na lista de forma organizada. */
     for (i = 0; i < TAM; i++) {
-        printf("---------- Pessoa %d ----------\n", i);
-        printf("Nome: %s\n", lista[i].nome);
+        printf("---------- Pessoa %d ----------\n", i + 1);
+        printf("Nome: %s", lista[i].nome);
         printf("Idade: %d\n", lista[i].idade);
         printf("Peso: %.2f\n", lista[i].peso);
     }
 
-    puts("----------------------");
+    puts("------------------------------");
 
     return 0;
 }
